606a707d5bc3b68541d5a881f0af5bcbb7a1e730
Nithil N <nithiln96@gmail.com>
2021-03-06 16:25:11 +0530

    Removed code that caused component rerender unnecessarily

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ClassRocket, FunctionalRocket } from './Rocket';
 import '../styles/_launchpad.scss';
 
 export default function LaunchPad() {
-  const [rerenderCount, triggerRerender] = useState(0);
 
-  setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
 
   return (
     <div className="launchpad">
